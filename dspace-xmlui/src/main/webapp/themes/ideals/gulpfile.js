48e89318c423d840354f62ee6c8dc92f401af5c1
Bill Ingram <waingram@gmail.com>
2015-04-16 15:55:44 -0500

    lots of changes to the sass compilation for the ideals theme

--- dspace-xmlui/src/main/webapp/themes/ideals/gulpfile.js
+++ dspace-xmlui/src/main/webapp/themes/ideals/gulpfile.js
@@ -14,6 +14,7 @@ var gulp = require('gulp'),
   notify = require('gulp-notify'),
   cache = require('gulp-cache'),
   livereload = require('gulp-livereload'),
+    runSequence = require('run-sequence'),
   del = require('del');
 
 // Paths - needed to find bootstrap adn fontawesome since they are downloaded by npm
@@ -22,15 +23,52 @@ var paths = {
   'fontawesome': 'node_modules/font-awesome'
 };
 
-// Compile Sass, Autoprefix and minify
-gulp.task('styles', function() {
-  return sass('src/scss/main.scss', {
+// Compile Bootstrap
+gulp.task('bootstrap', function() {
+  return sass(paths.bootstrap + '/stylesheets/_bootstrap.scss', {
     style: 'expanded',
     precision: '8',
     loadPath: [
-      paths.bootstrap + '/stylesheets',
+      paths.bootstrap + '/stylesheets'
+    ]
+  })
+      .pipe(rename({
+        basename: 'bootstrap'
+      }))
+      .pipe(gulp.dest('lib/css/'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(minifycss())
+      .pipe(gulp.dest('lib/css/'))
+      .pipe(notify({
+        message: 'Bootstrap task complete'
+      }));
+});
+
+// Compile Font Awesome
+gulp.task('fontawesome', function() {
+  return sass(paths.fontawesome + '/scss/font-awesome.scss', {
+    style: 'expanded',
+    loadPath: [
       paths.fontawesome + '/scss'
     ]
+  })
+      .pipe(gulp.dest('lib/css/'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(minifycss())
+      .pipe(gulp.dest('lib/css/'))
+      .pipe(notify({
+        message: 'Font-Awesome task complete'
+      }));
+});
+
+// Compile IDEALS styles, Autoprefix and minify
+gulp.task('ideals', function() {
+  return sass('src/scss/ideals.scss', {
+    style: 'expanded'
   })
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('lib/css'))
@@ -40,9 +78,30 @@ gulp.task('styles', function() {
     .pipe(minifycss())
     .pipe(gulp.dest('lib/css'))
     .pipe(notify({
-      message: 'Styles task complete'
+      message: 'IDEALS styles task complete'
     }));
 });
+
+// Compile print styles, Autoprefix and minify
+gulp.task('print', function() {
+  return sass('src/scss/print.scss', {
+    style: 'expanded'
+  })
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+      .pipe(gulp.dest('lib/css'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(minifycss())
+      .pipe(gulp.dest('lib/css'))
+      .pipe(notify({
+        message: 'Print styles task complete'
+      }));
+});
+
+gulp.task('styles', function() {
+  runSequence('bootstrap', 'fontawesome', 'ideals', 'print');
+})
 
 // Clean
 gulp.task('clean', function(cb) {
@@ -58,7 +117,9 @@ gulp.task('default', ['clean'], function() {
 gulp.task('watch', function() {
 
     // Watch .scss files
-    gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch('src/scss/*.scss', ['ideals']);
+    gulp.watch(paths.bootstrap + '/stylesheets/' + '/**/*.scss', ['bootstrap']);
+    gulp.watch(paths.fontawesome + '/scss/' + '/**/*.scss', ['fontawesome']);
 
     //// Watch .js files
     //gulp.watch('src/scripts/**/*.js', ['scripts']);
